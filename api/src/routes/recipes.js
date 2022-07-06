5041d902a2866c6f3b9ae80fe0148ae3fa92b888
MakoskiVictor <makoski.ed@gmail.com>
2022-07-06 05:35:52 -0300

    route get all recipes added

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,15 +1,26 @@
+const e = require('express');
 const { Router } = require('express');
 // Importar todos los routers;
-const {getRecipesApi} = require("../controllers/recipeControllers.js")
+const { getAllRecipes } = require("../controllers/recipeControllers.js")
 
 
 const router = Router();
 
 // Configurar los routers
 router.get("/", async (req, res, next)=>{
+    const {name} = req.query
     try {
-        const getApi = getRecipesApi()
-        res.status(200).json(getApi);
+        const getRecipe = await getAllRecipes();
+        console.log("SOY GET RECIPE", getRecipe)
+        if(name) {
+            //FILTRA DE TODAS LAS RECETAS, las que tengan el name igual al name del query
+            //AMBOS LOS BUSCAMOS EN MINUSCULAS PARA QUE MACHEE
+            const recipeName = await getRecipe.filter(r => r.name.toLowerCase().includes(name.toLowerCase()))
+            return res.status(200).json(recipeName);
+        } else {
+            //console.log("SOY GET RECIPE", asdd)
+            return res.status(200).json(getRecipe);
+        }
     } catch (error) {
         next(error)
     }
