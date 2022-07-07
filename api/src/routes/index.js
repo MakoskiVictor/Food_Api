9f3ecf8aed031bb5fec767541d7f50f92fdfcdda
MakoskiVictor <makoski.ed@gmail.com>
2022-07-07 19:31:57 -0300

    index updated

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const recipes = require("./recipes")
 const diets = require("./diets")
+const recipe = require("./recipePerId")
 
 const router = Router();
 
 router.use("/recipes", recipes);
+router.use("/recipe", recipe);
 router.use("/diets", diets);
 
 
