e738771971a3689d25e3acf94841dc5c7b07729e
MakoskiVictor <makoski.ed@gmail.com>
2022-07-06 21:06:35 -0300

    routes recipes updated

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,7 +1,7 @@
 const e = require('express');
 const { Router } = require('express');
 // Importar todos los routers;
-const { getAllRecipes } = require("../controllers/recipeControllers.js")
+const { getAllRecipes, postRecipe } = require("../controllers/recipeControllers.js")
 
 
 const router = Router();
@@ -11,18 +11,31 @@ router.get("/", async (req, res, next)=>{
     const {name} = req.query
     try {
         const getRecipe = await getAllRecipes();
-        console.log("SOY GET RECIPE", getRecipe)
+        //console.log("SOY GET RECIPE", getRecipe)
         if(name) {
             //FILTRA DE TODAS LAS RECETAS, las que tengan el name igual al name del query
             //AMBOS LOS BUSCAMOS EN MINUSCULAS PARA QUE MACHEE
+            //EL INCLUDES ME BUSCA TAMBIEN LOS NOMBRES PARECIDOS, POR ESO USAMOS INCLUDES Y NO ===
             const recipeName = await getRecipe.filter(r => r.name.toLowerCase().includes(name.toLowerCase()))
             return res.status(200).json(recipeName);
         } else {
-            //console.log("SOY GET RECIPE", asdd)
+            //console.log("SOY GET RECIPE", getRecipe)
             return res.status(200).json(getRecipe);
         }
     } catch (error) {
         next(error)
+    }
+})
+
+//POST RECIPE
+
+router.post("/", async (req, res, next)=>{
+    const { name, summary, healthScore, steps, image, diets } = req.body;
+    try {
+        const recipeDb = await postRecipe(name, summary, healthScore, steps, image, diets);
+        return res.status(201).json(recipeDb);
+    } catch (error) {
+        next(error);
     }
 })
 
