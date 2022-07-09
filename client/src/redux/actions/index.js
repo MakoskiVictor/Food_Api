380648fa9a4a26cccc0bd4c10d5f74a7b06e849c
MakoskiVictor <makoski.ed@gmail.com>
2022-07-08 22:05:38 -0300

    redux

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,73 @@
+import { legacy_createStore } from "redux";
+
+export const FETCH_ALL_RECIPES = "FETCH_ALL_RECIPES";
+export const FETCH_RECIPE_NAME = "FETCH_RECIPE_NAME";
+export const FETCH_RECIPE_ID = "FETCH_RECIPE_ID";
+export const POST_RECIPE = "POST_RECIPE";
+
+export function fetchAllRecipes () {
+    return function(dispatch) {
+        fetch(`http://localhost:3001/api/recipes`)
+        .then(response => response.json)
+        .then((recipes)=>{
+            dispatch({
+                type: FETCH_ALL_RECIPES,
+                payload: recipes
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+};
+
+export function fetchRecipeName(name) {
+    return function(dispatch){
+        fetch(`http://localhost:3001/api/recipes?name=${name}`)
+        .then(response => response.json)
+        .then((recipe) => {
+            dispatch({
+                type: FETCH_RECIPE_NAME,
+                payload: recipe
+            })
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+};
+
+export function fetchRecipeId(id) {
+    return function(dispatch) {
+        fetch(`http://localhost:3001/api/recipes/${id}`)
+        .then(response => response.json)
+        .then((recipe) =>{
+            dispatch({
+                type: FETCH_RECIPE_ID,
+                payload: recipe
+            })
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+};
+
+export function postRecipe(data) {
+    return function(dispatch) {
+        fetch(`http://localhost:3001/api/recipes`, {
+            method: "POST",
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json)
+        .then((response)=>{
+            dispatch({
+                type: POST_RECIPE,
+                payload: response
+            })
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+};
---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,33 @@
+//IMPORTAMOS LAS ACTIONS
+import { FETCH_ALL_RECIPES, FETCH_RECIPE_NAME, FETCH_RECIPE_ID, POST_RECIPE } from "../actions";
+
+//SETEAMOS EL/LOS ESTADO/S
+const initialState = {
+    allRecipes: []
+};
+
+export default function reducer (state = initialState, action) {
+    switch (action.type) {
+        case FETCH_ALL_RECIPES:
+            return {
+                ...state,
+                allRecipes: action.payload
+            }
+        case FETCH_RECIPE_NAME:
+            return {
+                ...state,
+                allRecipes: action.payload
+            }
+        case FETCH_RECIPE_ID:
+            return {
+                ...state,
+                allRecipes: action.payload
+            }
+        case POST_RECIPE:
+            return {
+                ...state,
+            }
+        default:
+            return state
+    }
+};
