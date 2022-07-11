cabd31bc2934f401c2c6e135c44a058d0aff8c3b
MakoskiVictor <makoski.ed@gmail.com>
2022-07-11 16:10:11 -0300

    store fixed

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import thunk from "redux-thunk"; //NOS PERMITE CREAR ACTIONS CREATORS QUE DEVUELVAN UNA FUNCTION
+//LO IMPORTANTE ES QUE ESTAS FUNCTIONS PUEDEN OPERAR DE MANERA ASINCRONA
 import reducer from "../reducer"
 
 //CREAR LA STORE
