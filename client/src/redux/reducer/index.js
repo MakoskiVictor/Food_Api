e7b930c2d14b26876e233639fbf5f3bb12825b56
MakoskiVictor <makoski.ed@gmail.com>
2022-07-12 17:16:26 -0300

    reducer by diets added

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,5 +1,5 @@
 //IMPORTAMOS LAS ACTIONS
-import { FETCH_ALL_RECIPES, FETCH_RECIPE_NAME, FETCH_RECIPE_ID, POST_RECIPE } from "../actions";
+import { FETCH_ALL_RECIPES, FETCH_RECIPE_NAME, FETCH_RECIPE_ID, POST_RECIPE, FILTER_BY_DIET } from "../actions";
 
 //SETEAMOS EL/LOS ESTADO/S
 const initialState = {
@@ -26,6 +26,15 @@ export default function reducer (state = initialState, action) {
         case POST_RECIPE:
             return {
                 ...state,
+            }
+        case FILTER_BY_DIET:
+            const allDiets = state.allRecipes;
+            const filteredDiets = action.payload === "All" ? allDiets : allDiets.filter(d => d.diets.includes(action.payload));
+            console.log("SOY REDUCER", filteredDiets)
+            console.log("SOY REDUCER", state.allRecipes)
+            return {
+                ...state,
+                allRecipes: filteredDiets
             }
         default:
             return state
