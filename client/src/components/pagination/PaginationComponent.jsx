b66612ee5540548a6dcabcd62e8e06107db79c25
MakoskiVictor <makoski.ed@gmail.com>
2022-07-12 17:15:13 -0300

    pagination fixed

--- client/src/components/pagination/PaginationComponent.jsx
+++ client/src/components/pagination/PaginationComponent.jsx
@@ -5,7 +5,7 @@ export default function PaginationComponent({recipesPerPage, allRecipes, paginat
     const pageNumbers = [];
     //EL CICLO FOR ME REDONDEA LA CANTIDAD DE PAGINAS QUE NECESITO
     for(let i=0; i<=Math.ceil(allRecipes/recipesPerPage); i++) {
-        pageNumbers.push(i);
+        pageNumbers.push(i+1); // el i+1 es para que inicie en la pag 1, si no lo ponemos inicia en pag 0
     }
 
     return(
@@ -13,7 +13,7 @@ export default function PaginationComponent({recipesPerPage, allRecipes, paginat
             <ul className="paginated">
                 { pageNumbers?.map((number)=>{
                     return( 
-                    <li className="number">
+                    <li className="number" key={number}>
                             <button className="pageButton" onClick={() => paginated(number)}>{number}</button>
                     </li>
                 )})}
