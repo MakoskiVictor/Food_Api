b1da20d5c4f4380ca0e211dc83655c12a6ff0e13
MakoskiVictor <makoski.ed@gmail.com>
2022-07-13 20:22:28 -0300

    pagination minor fixed

--- client/src/components/pagination/PaginationComponent.jsx
+++ client/src/components/pagination/PaginationComponent.jsx
@@ -6,7 +6,7 @@ export default function PaginationComponent({recipesPerPage, allRecipes, paginat
     //EL CICLO FOR ME REDONDEA LA CANTIDAD DE PAGINAS QUE NECESITO
     for(let i=0; i<=Math.ceil(allRecipes/recipesPerPage); i++) {
         pageNumbers.push(i+1); // el i+1 es para que inicie en la pag 1, si no lo ponemos inicia en pag 0
-    }
+    };
 
     return(
         <nav className="pagination">
@@ -20,4 +20,4 @@ export default function PaginationComponent({recipesPerPage, allRecipes, paginat
             </ul>
         </nav>
     )
-}
+};
