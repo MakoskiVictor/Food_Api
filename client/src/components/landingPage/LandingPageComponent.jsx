b12a70c5297e64d92d08370855ded08d1986047f
MakoskiVictor <makoski.ed@gmail.com>
2022-07-21 02:00:19 -0300

    landing css

--- client/src/components/landingPage/LandingPageComponent.jsx
+++ client/src/components/landingPage/LandingPageComponent.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../landingPage/LandingPageComponent.css"
 
 export default function LandingPageComponent () { 
     return (
-        <div>
-            <h2>Landing Page</h2>
+        <div className="landingP">
             <Link to="/home">
-                <button>To Home</button>
+                <button className="landingBtn">To Home</button>
             </Link>
         </div>
     )
