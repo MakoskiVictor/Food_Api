38f7abd1c5cb0b295304c1836db58457f1b5bd31
MakoskiVictor <makoski.ed@gmail.com>
2022-07-21 01:58:58 -0300

    card component css

--- client/src/components/card/CardComponent.jsx
+++ client/src/components/card/CardComponent.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { legacy_createStore } from "redux";
+import { Link } from "react-router-dom";
+import "../card/CardComponent.css"
 
 export default function CardComponent ({name, summary, healthScore, steps, image, diets, id}) {
     return(
-        <div>
-            <h3>Name: {name}</h3>
-            <img src={image? image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABE23O3ZT5uGShVBbMEMpQM3u2M3f4WldUA&usqp=CAU"} alt="img not found" width="250px" height="250px" />
-            <h4>ID: {id}</h4>
-            <h4>Health Score: {healthScore}</h4>
-            <ul>Diets: 
-                {diets && diets.map((r)=> {
-                    return( 
-                        <li key={r}>{r}</li>
-                    )
-                }) 
-                }
-            </ul>
+        <div className="card">
+            <Link to={`/details/${id}`} className="link">
+                <h3 className="name">{name}</h3>
+                <img src={image? image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABE23O3ZT5uGShVBbMEMpQM3u2M3f4WldUA&usqp=CAU"} alt="img not found" width="250px" height="250px" />
+                <h4>ID: {id}</h4>
+                <h4>Health Score: {healthScore}</h4>
+                <ul>Diets: 
+                    {diets && diets.map((r)=> {
+                        return( 
+                            <li key={r} className="diets">{`-${r}`}</li>
+                        )
+                    }) 
+                    }
+                </ul>
+            </Link>
         </div>
     )
 }
