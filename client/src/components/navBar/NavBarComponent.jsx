df899d69c69ebbd9f708ebb614e64abf65068f67
MakoskiVictor <makoski.ed@gmail.com>
2022-07-21 02:00:43 -0300

    navbar css

--- client/src/components/navBar/NavBarComponent.jsx
+++ client/src/components/navBar/NavBarComponent.jsx
@@ -1,49 +1,32 @@
-import React, { useState, } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import chefImg from "../../assets/chefImg.jpg"
 import { Link } from "react-router-dom";
 import "./NavBarComponent.css"
-import { fetchRecipeName } from "../../redux/actions";
+import SearchBarComponent from "../searchBar/SearchBarComponent";
 
 export default function NavBarComponent () {
 
-    //SETEO EL ESTADO DE LA SEARCHBAR
-    const [search, setSearch] = useState("");
-    const dispatch = useDispatch();
-
-
-    //RENDERIZO EL ESTADO
-    const onInputChange = (e)=> {
-        e.preventDefault();
-        setSearch(e.target.value)
-    }
-
-    //DESPACHO LA BUSQUEDA
-    const onSubmit = (e)=> {
-        e.preventDefault();
-        dispatch(fetchRecipeName(search))
-        setSearch("");
-    }
-
-
     return(
         <div className="NavBarComplete">
-            <div>
-                <Link to={"/home"}>
-                    <img src={chefImg} alt="Not found" width="50px" height="50px" />
+            <div className="logo">
+                <Link to={"/home"} className="link1">
+                    <img src={chefImg} alt="Not found" width="100px" height="100px" />
                 </Link>
             </div>
             <br />
-            <div>
-                <Link  to={"/create"}>
+            <div className="button">
+                <Link  to={"/create"} className="link1">
                     <div>CREATE RECIPE</div>
                 </Link>
             </div>
             <br />
-            <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Search Recipe" onChange={onInputChange} value={search}/>
-                <input type="submit" value="Search" />
-            </form>
+            <div className="button">
+            <Link  to={"/home"} className="link1">
+                    <div>HOME</div>
+                </Link>
+            </div>
+            <br />
+            <SearchBarComponent/>
 
 
         </div>
