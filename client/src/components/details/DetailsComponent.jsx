22775dee698681d1f9fe073f4cdd26fc49cac459
Makoski Víctor <makoski.ed@gmail.com>
2022-10-27 22:10:30 -0300

    added Cloudinary and fixed diets bug in details

--- client/src/components/details/DetailsComponent.jsx
+++ client/src/components/details/DetailsComponent.jsx
@@ -16,39 +16,37 @@ export default function DetailsComponent (props) {
         dispatch(deleteDetails())
     }, [dispatch]);
 
-/*     const onChange = () => {
-        dispatch(deleteDetails())
-    } */
 
     return(
         <div>
             <NavBarComponent/>
             <br />
             <br />
+            {detail.id ?
             <div className="details">
-                {detail.length > 0?
+                {detail.id.length > 30?
                     <div>
-                        <h1 className="nameDet">{detail[0].name}</h1>
+                        <h1 className="nameDet">{detail.name}</h1>
                         <br />
-                        <img className="imageDet" src={detail[0].image? detail[0].image 
+                        <img className="imageDet" src={detail.image? detail.image 
                             : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABE23O3ZT5uGShVBbMEMpQM3u2M3f4WldUA&usqp=CAU"} 
                             alt="img not found" width="500px" height="500px" />
                             <br />
-                <h4>ID: {detail[0].id}</h4>
+                <h4>ID: {detail.id}</h4>
                 <br />
-                <h4 className="score">Health Score: {detail[0].healthScore}</h4>
+                <h4 className="score">Health Score: {detail.healthScore}</h4>
                 <br />
                 <div className="summaryDet">
-                    <p>Summary: {detail[0].summary? detail[0].summary : "Nothing here"} </p>
+                    <p>Summary: {detail.summary? detail.summary : "Nothing here"} </p>
                 </div>
                 <br />
-                <p className="steps">Steps: {detail[0].steps} </p>
+                <p className="steps">Steps: {detail.steps} </p>
                 <br />
                 <div className="dietsDet">
                     <ul>Diets: 
-                        {detail[0].diets && detail[0].diets.map((r)=> {
+                        {detail.diets && detail.diets.map((r, i)=> {
                             return( 
-                                <p key={r}>{r}</p>
+                                <p key={i}>{r.name}</p>
                             )
                         }) 
                         }
@@ -56,9 +54,38 @@ export default function DetailsComponent (props) {
                 </div>
                     </div>
                     :
-                    <p>LOADING...</p>
+                    <div>
+                    <h1 className="nameDet">{detail.name}</h1>
+                    <br />
+                    <img className="imageDet" src={detail.image? detail.image 
+                        : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABE23O3ZT5uGShVBbMEMpQM3u2M3f4WldUA&usqp=CAU"} 
+                        alt="img not found" width="500px" height="500px" />
+                        <br />
+            <h4>ID: {detail.id}</h4>
+            <br />
+            <h4 className="score">Health Score: {detail.healthScore}</h4>
+            <br />
+            <div className="summaryDet">
+                <p>Summary: {detail.summary? detail.summary : "Nothing here"} </p>
+            </div>
+            <br />
+            <p className="steps">Steps: {detail.steps} </p>
+            <br />
+            <div className="dietsDet">
+                <ul>Diets: 
+                    {detail.diets && detail.diets.map((r, i)=> {
+                        return( 
+                            <p key={i}>{r}</p>
+                        )
+                    }) 
+                    }
+                </ul>
+            </div>
+                </div>
                 }
             </div>
+            :
+            <p>LOADING...</p> }
             <div>
                 <Link to="/home">
                     <button className="back">Go Back</button>
--- client/src/components/postRecipe/PostRecipeComponent.jsx
+++ client/src/components/postRecipe/PostRecipeComponent.jsx
@@ -4,180 +4,261 @@ import { Link, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllRecipes, fetchDiets, postRecipe } from "../../redux/actions";
 import NavBarComponent from "../navBar/NavBarComponent.jsx";
-import "../postRecipe/PostRecipeComponent.css"
+import axios from "axios";
+import "../postRecipe/PostRecipeComponent.css";
 
-export default function PostRecipeComponent () {
+export default function PostRecipeComponent() {
+  const diets = useSelector((state) => state.diets);
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState({});
 
-    const diets = useSelector((state)=> state.diets);
-    const dispatch = useDispatch();
-    const [errors, setErrors] = useState({});
+  const c = console.log.bind(document);
 
-    
-    const history = useHistory();
-    
-    useEffect(()=> {
-        dispatch(fetchAllRecipes())
-        dispatch(fetchDiets())
-    }, [dispatch])
-    
-    const [input, setInput] = useState({
-        name: "",
-        summary: "",
-        healthScore: 0,
-        steps: "",
-        image: "",
-        diets: []
-    })
+  const history = useHistory();
 
+  useEffect(() => {
+    dispatch(fetchAllRecipes());
+    dispatch(fetchDiets());
+  }, [dispatch]);
 
-    //VALIDACIONES
-    let validateName = /^[a-zA-Z\s]+$/;
-    let validateUrl = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/;
-    const validate = (input) => {
+  //ESTADO DE LA IMAGEN
+  const [imageSelected, setImageSelected] = useState("");
+
+  //ESTADO INPUTS
+  const [input, setInput] = useState({
+    name: "",
+    summary: "",
+    healthScore: "",
+    steps: "",
+    image: "",
+    diets: [],
+  });
+
+  c(input)
+  c(imageSelected)
+
+  //VALIDACIONES
+  let validateName = /^[a-zA-Z\s]+$/;
+  let validateUrl = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/;
+  const validate = (input) => {
     let errors = {};
     if (!input.name.length) {
-        errors.name = "This field cannot be empty";
+      errors.name = "This field cannot be empty";
     }
     if (!validateName.test(input.name)) {
-        errors.name = "Special characters or numbers are not allowed";
+      errors.name = "Special characters or numbers are not allowed";
     }
-    if (input.image && !validateUrl.test(input.image)) {
-        errors.image = "This is not a valid URL";
+    /* if (input.image && !validateUrl.test(input.image)) {
+      errors.image = "This is not a valid URL";
     }
     if (input.image.length > 100) {
-        errors.image = "This URL is too long";
-    }
+      errors.image = "This URL is too long";
+    } */
     if (!input.summary.length) {
-        errors.summary = "This field cannot be empty";
+      errors.summary = "This field cannot be empty";
     }
     if (input.summary.length < 40) {
-        errors.summary = "This field must be at least 40 characters";
+      errors.summary = "This field must be at least 40 characters";
     }
     if (input.healthScore < 1 || input.healthScore > 100) {
-        errors.healthScore = "Number required. Must be a number between 1-100";
+      errors.healthScore = "Number required. Must be a number between 1-100";
     }
     if (!input.steps.length) {
-        errors.steps = "This field cannot be empty";
+      errors.steps = "This field cannot be empty";
     }
     if (input.steps.length < 80) {
-        errors.steps = "This field must be longer than 80 characters";
+      errors.steps = "This field must be longer than 80 characters";
     }
     return errors;
-    };
+  };
 
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
 
+  const handleCheckbox = (e) => {
+    if (e.target.checked && !input.diets.includes(e.target.value)) {
+      setInput({
+        ...input,
+        diets: [...input.diets, e.target.value],
+      });
+    } else if (!e.target.checked) {
+      setInput({
+        ...input,
+        diets: input.diets.filter((d) => d !== e.target.value),
+      });
+    }
+  };
 
-    const handleChange = (e) => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (Object.keys(errors).length === 0 && input.diets.length > 0) {
+      if (imageSelected) {
+          c("entró a cloudinary")
+        let imgCloudinary;
+        const formData = new FormData();
+        formData.append("file", imageSelected);
+        formData.append("upload_preset", "goctl1il");
+
+        try {
+            await axios.post(
+                "https://api.cloudinary.com/v1_1/dzr5xulsx/image/upload",
+                formData
+              )
+            .then((response) => {
+                imgCloudinary = response.data.secure_url;
+            })
+            .then( async () => {
+                await axios.post(`http://localhost:3001/api/recipes`, {
+                  name: input.name,
+                  summary: input.summary,
+                  healthScore: input.healthScore,
+                  steps: input.steps,
+                  image: imgCloudinary,
+                  diets: input.diets,
+                });
+            })
+        } catch (error) {
+            console.error(error)
+        }
+      } else {
+        c("NO entró a cloudinary")
+        dispatch(postRecipe(input));
+        alert("Recipe created");
         setInput({
-            ...input,
-            [e.target.name] : e.target.value
+          name: "",
+          summary: "",
+          healthScore: 0,
+          steps: "",
+          image: "",
+          diets: [],
         });
-        setErrors(
-            validate({
-                ...input,
-                [e.target.name]: e.target.value,
-            })
-        )
-    };
+        history.push("/home");
+      }
+    } else {
+      alert("All fields must be completed");
+    }
+  };
 
-
-    const handleCheckbox = (e) => {
-        if(e.target.checked && !input.diets.includes(e.target.value)) {
-            setInput({
-                ...input,
-                diets: [...input.diets, e.target.value]
-            })
-        } else if (!e.target.checked) {
-            setInput({
-                ...input,
-                diets: input.diets.filter((d) => d !== e.target.value)
-            })
-        }
-    };
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (Object.keys(errors).length === 0 && input.diets.length > 0) {
-            dispatch(postRecipe(input))
-            alert("Recipe created");
-            setInput({
-                name: "",
-                summary: "",
-                healthScore: 0,
-                steps: "",
-                image: "",
-                diets: []
-            })
-            history.push("/home")
-        } else {
-            alert("All fields must be completed");
-        }
-    };
-
-    return(
+  return (
+    <div>
+      <NavBarComponent />
+      <h3 className="title">CREATE RECIPE</h3>
+      <form onSubmit={(e) => onSubmit(e)} className="form">
         <div>
-            <NavBarComponent/>
-            <h3 className="title">CREATE RECIPE</h3>
-            <form onSubmit={(e)=>onSubmit(e)} className="form">
-                <div>
-                    <label>Name: </label>
-                    <input className="input" type="text" placeholder="Name" autoComplete="off" 
-                    name="name" value={input.name} onChange={(e)=> handleChange(e)} />
-                    {errors.name && <p>{errors.name}</p>}
-                </div>
-                <br />
-                <div>
-                    <label className="sumSteps" htmlFor="summary">Summary: </label>
-                    <textarea className="textbox" type="text" placeholder="Summary" autoComplete="off" 
-                    name="summary" value={input.summary} onChange={(e)=> handleChange(e)}/>
-                    {errors.summary && <p>{errors.summary}</p>}
-                </div>
-                <br />
-                <div>
-                    <label htmlFor="healthScore">Health Score: </label>
-                    <input className="inputScore" type="number" placeholder="Ingrese Number" min="1" max="100" 
-                    name="healthScore" value={input.healthScore} autoComplete="off" onChange={(e)=> handleChange(e)}/>
-                    {errors.healthScore && (<p>{errors.healthScore}</p>)}
-                </div>
-                <br />
-                <div>
+          <label>Name: {" "}</label>
+          <input
+            className="input"
+            type="text"
+            placeholder="Name"
+            autoComplete="off"
+            name="name"
+            value={input.name}
+            onChange={(e) => handleChange(e)}
+          />
+          {errors.name && <p>{errors.name}</p>}
+        </div>
+        <br />
+        <div>
+          <label className="sumSteps" htmlFor="summary">
+            Summary:{" "}
+          </label>
+          <textarea
+            className="textbox"
+            type="text"
+            placeholder="Summary"
+            autoComplete="off"
+            name="summary"
+            value={input.summary}
+            onChange={(e) => handleChange(e)}
+          />
+          {errors.summary && <p>{errors.summary}</p>}
+        </div>
+        <br />
+        <div>
+          <label htmlFor="healthScore">Health Score: {" "}</label>
+          <input
+            className="inputScore"
+            type="number"
+            placeholder="Ingrese Number"
+            min="1"
+            max="100"
+            name="healthScore"
+            value={input.healthScore}
+            autoComplete="off"
+            onChange={(e) => handleChange(e)}
+          />
+          {errors.healthScore && <p>{errors.healthScore}</p>}
+        </div>
+        <br />
+        {/* <div>
                     <label htmlFor="image">Image: </label>
                     <input className="input" type="url" placeholder="URL...(Optional)" autoComplete="off" name="image" 
                     value={input.image} onChange={(e)=> handleChange(e)}/>
                     {errors.image && <p>{errors.image}</p>}
-                </div>
-                <br />
-                <div>
-                    <label className="sumSteps" htmlFor="steps">Steps: </label>
-                    <textarea className="textbox" type="textarea" placeholder="Ingrese steps" autoComplete="off" 
-                    name="steps" value={input.steps} onChange={(e)=> handleChange(e)}/>
-                    {errors.steps && (<p>{errors.steps}</p>)}
-                </div>
-                <br />
-                <div className="checkbox">
-                    <label htmlFor="diets">Select Diets</label>
-                    <br />
-                    <br />
-                    {diets.map((d)=> (
-                        <label htmlFor={d.name} key={d.name}>
-                        <input type="checkbox" name="diets" value={d.name} onChange={(e)=> handleCheckbox(e)}/>
-                        {d.name} {" "}
-                        </label>
-                    ))}
-                </div>
-                <br />
-                <div>
-                    <button className="create">Create Recipe</button>
-                </div>
-            </form>
-            <br />
-            <div>
-                <Link to="/home">
-                    <button className="cancel">CANCEL</button>
-                </Link>
-            </div>
-
-        
+                </div> */}
+        <div className="inputsContainerImg">
+          <label className="labelImg">Image (Optional): </label>
+          <input
+            type="file"
+            onChange={(e) => {
+              setImageSelected(e.target.files[0]);
+            }}
+          />
         </div>
-    )
+        <br />
+        <div>
+          <label className="sumSteps" htmlFor="steps">
+            Steps:{" "}
+          </label>
+          <textarea
+            className="textbox"
+            type="textarea"
+            placeholder="Ingrese steps"
+            autoComplete="off"
+            name="steps"
+            value={input.steps}
+            onChange={(e) => handleChange(e)}
+          />
+          {errors.steps && <p>{errors.steps}</p>}
+        </div>
+        <br />
+        <div className="checkbox">
+          <label htmlFor="diets">Select Diets {" "}</label>
+          <br />
+          <br />
+          {diets.map((d) => (
+            <label htmlFor={d.name} key={d.name}>
+              <input
+                type="checkbox"
+                name="diets"
+                value={d.name}
+                onChange={(e) => handleCheckbox(e)}
+              />
+              {d.name}{" "}
+            </label>
+          ))}
+        </div>
+        <br />
+        <div>
+          <button className="create">Create Recipe</button>
+        </div>
+      </form>
+      <br />
+      <div>
+        <Link to="/home">
+          <button className="cancel">CANCEL</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
