8e43aa32fd2c3032cde4a13efc4b7927e77aaffb
Makoski Víctor <makoski.ed@gmail.com>
2022-11-08 16:35:15 -0300

    changes for deploy

--- client/src/constantes.js
+++ client/src/constantes.js
@@ -1,2 +1,3 @@
 export const FRONT_URL = "http://localhost:3000";
-export const BACK_URL =  "http://localhost:3001";
+export const BACK_URL =  "https://foodapiback.onrender.com/";
+/* export const BACK_URL =  "http://localhost:3001"; */
